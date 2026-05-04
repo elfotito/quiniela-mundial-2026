0cf12ee347219fa442bb76dd691a44a8011fde9c
Viktor <elfotito@gmail.com>
2026-05-04 11:55:30 -0400

    REMONTADA EN MUNICH

--- frontend/js/predicciones.js
+++ frontend/js/predicciones.js
@@ -783,6 +783,23 @@ setTimeout(() => {
     });
 })();
 
+const backToTopBtn = document.getElementById('backToTop');
+
+        window.addEventListener('scroll', function() {
+            
+            if (window.scrollY > 300) {
+                backToTopBtn.classList.add('show');
+            } else {
+                backToTopBtn.classList.remove('show');
+            }
+        });
+
+        backToTopBtn.addEventListener('click', function() {
+            window.scrollTo({
+                top: 0,
+                behavior: 'smooth'
+            });
+        });
 
 function logout() {
     if (confirm('¿Estás seguro de que quieres salir?')) auth.logout();
