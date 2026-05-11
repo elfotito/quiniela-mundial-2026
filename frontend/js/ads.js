6246a231670f46fb88bd652b6446531db5548f83
Viktor <elfotito@gmail.com>
2026-05-11 10:25:37 -0400

    AGARRATE AGARRATE

--- frontend/js/ads.js
+++ frontend/js/ads.js
@@ -1,21 +1,23 @@
 // widget-ads.js
 // ─── 1:1 (260x260) ───────────────────────────────────────
-const ADS_11 = [
+const AD_POOL = {
+  // ─── 1:1 (260x260) ───────────────────────────────────────
+'11' : [
   { url: 'https://i.ibb.co/zVb3CqPk/Save-Clip-App-643561767-17977242236830415-4476175652748288014-n.webp', link: 'https://www.instagram.com/unicardio.ha/' },
   { url: 'https://i.ibb.co/jP97FC5M/Save-Clip-App-651932567-18102108917497080-5880069149386066025-n.webp', link: 'https://www.instagram.com/unicardio.ha/' },
   { url: 'https://i.ibb.co/SXF8b6PN/Save-Clip-App-652773881-18087064778269663-1733593774225891798-n.webp', link: 'https://www.instagram.com/unicardio.ha/' },
   { url: 'https://i.ibb.co/LKyHTd7/Save-Clip-App-655086237-18078404945574082-8845079464837833066-n-1.webp', link: 'thttps://www.instagram.com/deliverysorpresa/' },
   { url: 'https://i.ibb.co/Pv6fMMLv/Save-Clip-App-658474710-18370531537202009-7154433570683986532-n.webp', link: 'thttps://www.instagram.com/deliverysorpresa/' },
   { url: 'https://i.ibb.co/rRKfbtpH/Save-Clip-App-656057688-18117165637712782-2735713681201846829-n.webp', link: 'thttps://www.instagram.com/deliverysorpresa/' },
-];
+],
 
 // ─── 4:3 (260x346) ───────────────────────────────────────
-const ADS_43 = [
+'43' : [
   { url: 'https://i.ibb.co/YT1bj33K/Gemini-Generated-Image-8hk19e8hk19e8hk1.png', link: 'https://www.instagram.com/dcarrisan' },
   { url: 'https://i.ibb.co/qYQ1jYmZ/Gemini-Generated-Image-fgsxjlfgsxjlfgsx.png', link: 'https://www.instagram.com/dcarrisan' },
   { url: 'https://i.ibb.co/PvpKL0gS/Gemini-Generated-Image-81pq5l81pq5l81pq.png', link: 'https://www.instagram.com/dcarrisan' },
-];
-
+],
+};
 
 const AD_INTERVAL = 8000; // ms
 
