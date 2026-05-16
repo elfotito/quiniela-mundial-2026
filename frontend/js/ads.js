335411ca3b43e7571b57092fc7a1df484036fb85
Viktor <elfotito@gmail.com>
2026-05-16 16:16:14 -0400

    AGARRATE AGARRATE

--- frontend/js/ads.js
+++ frontend/js/ads.js
@@ -4,8 +4,8 @@ const AD_POOL = {
   // ─── 1:1 (260x260) ───────────────────────────────────────
 '11' : [
   { url: 'https://i.ibb.co/pBcBXc8w/unicardio.png', link: 'https://www.instagram.com/unicardio.ha/' },
-  { url: 'https://i.ibb.co/jP97FC5M/Save-Clip-App-651932567-18102108917497080-5880069149386066025-n.webp', link: 'https://www.instagram.com/unicardio.ha/' },
-  { url: 'https://i.ibb.co/SXF8b6PN/Save-Clip-App-652773881-18087064778269663-1733593774225891798-n.webp', link: 'https://www.instagram.com/unicardio.ha/' },
+  { url: 'https://i.ibb.co/pvztDpJL/unbicardio2.png', link: 'https://www.instagram.com/unicardio.ha/' },
+  { url: 'https://i.ibb.co/tMYcVDS2/labcelina.png', link: 'https://www.instagram.com/unicardio.ha/' },
   { url: 'https://i.ibb.co/LKyHTd7/Save-Clip-App-655086237-18078404945574082-8845079464837833066-n-1.webp', link: 'https://www.instagram.com/deliverysorpresa/' },
   { url: 'https://i.ibb.co/Pv6fMMLv/Save-Clip-App-658474710-18370531537202009-7154433570683986532-n.webp', link: 'https://www.instagram.com/deliverysorpresa/' },
   { url: 'https://i.ibb.co/rRKfbtpH/Save-Clip-App-656057688-18117165637712782-2735713681201846829-n.webp', link: 'https://www.instagram.com/deliverysorpresa/' },
