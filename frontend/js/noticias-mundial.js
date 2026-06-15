8542fa9fa7d25ba55eafe12db7fe987b273785ea
Viktor <elfotito@gmail.com>
2026-06-15 11:46:07 -0400

    Alemania solo golea a equipos chicos

--- frontend/js/noticias-mundial.js
+++ frontend/js/noticias-mundial.js
@@ -19,13 +19,13 @@ const NOTICIAS_MUNDIAL = [
   },
     {
     titulo: "🇺🇾 José María Giménez, un joven experimentado en la nueva era de Uruguay",
-    imagen: "https://digitalhub.fifa.com/transform/412f332c-ee45-46c7-b45a-1d2c6c418e98/FWC-Jose-Maria-Gimenez-Uruguay?focuspoint=0.58,0.35&io=transform:fill,aspectratio:4x3,width:120&quality=75",
+    imagen: "https://digitalhub.fifa.com/transform/412f332c-ee45-46c7-b45a-1d2c6c418e98/FWC-Jose-Maria-Gimenez-Uruguay?focuspoint=0.58,0.35&io=transform:fill,aspectratio:4x3,width:960&quality=75",
     categoria: "REFERENTE",
     url: "#"
   },
     {
     titulo: "🇪🇦 España arranca la conquista de la segunda estrella ante Cabo Verde",
-    imagen: "https://digitalhub.fifa.com/transform/bd0a6adc-adc8-4421-8332-f578ecbcf26b/Mikel-Oyarzabal-celebrates-scoring-for-Spain-against-Peru-at-the-Estadio-Cuauhtemoc-in-Puebla-in-a-FIFA-World-Cup-2026-warm-up-friendly?focuspoint=0.51,0.23&io=transform:fill,aspectratio:4x3,width:120&quality=75",
+    imagen: "https://digitalhub.fifa.com/transform/bd0a6adc-adc8-4421-8332-f578ecbcf26b/Mikel-Oyarzabal-celebrates-scoring-for-Spain-against-Peru-at-the-Estadio-Cuauhtemoc-in-Puebla-in-a-FIFA-World-Cup-2026-warm-up-friendly?focuspoint=0.51,0.23&io=transform:fill,aspectratio:4x3,width:960&quality=75",
     categoria: "BOTA DE ORO",
     url: "#"
   }
