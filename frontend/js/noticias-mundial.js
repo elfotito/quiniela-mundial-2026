2fbbc9e0bba1e9f37461fbc6f932cbddf8cfd218
Viktor <elfotito@gmail.com>
2026-06-15 11:27:03 -0400

    Alemania solo golea a equipos chicos

--- frontend/js/noticias-mundial.js
+++ frontend/js/noticias-mundial.js
@@ -12,45 +12,21 @@
 
 const NOTICIAS_MUNDIAL = [
 {
-    titulo: "🇰🇷 Corea del sur le da vuelta a Rep Checa y asegura sus 3 puntos vitales",
-    imagen: "https://a4.espncdn.com/combiner/i?img=%2Fphoto%2F2026%2F0612%2Fr1671753_1296x729_16%2D9.jpg&w=1140&cquality=40&format=jpg",
-    categoria: "PARTIDO",
+    titulo: "🇪🇦 España y una etiqueta que no pesa",
+    imagen: "https://digitalhub.fifa.com/transform/200d531a-ccda-477c-9f55-a5796df2d195/Spain-Training-And-Press-Conference-FIFA-World-Cup-2026?&io=transform:fill,aspectratio:4x3,width:960&quality=75",
+    categoria: "RUEDA DE PRENSA",
     url: "#"
   },
     {
-    titulo: "🇲🇽 México supera a Sudáfrica y sonríe en el debut",
-    imagen: "https://a.espncdn.com/combiner/i?img=%2Fphoto%2F2026%2F0611%2Fr1671534_1296x729_16%2D9.jpg",
-    categoria: "PARTIDO",
+    titulo: "🇺🇾 José María Giménez, un joven experimentado en la nueva era de Uruguay",
+    imagen: "https://digitalhub.fifa.com/transform/412f332c-ee45-46c7-b45a-1d2c6c418e98/FWC-Jose-Maria-Gimenez-Uruguay?focuspoint=0.58,0.35&io=transform:fill,aspectratio:4x3,width:120&quality=75",
+    categoria: "REFERENTE",
     url: "#"
   },
     {
-    titulo: "🏴󠁧󠁢󠁥󠁮󠁧󠁿 ¿Kane ganara la bota de oro por segunda vez? Seria el primero en hacerlo.",
-    imagen: "https://digitalhub.fifa.com/transform/38f4af27-b519-4fde-a6d0-44d339831b12/Harry-Kane-celebrates-after-England-beat-Serbia-in-European-qualifying-for-the-FIFA-World-Cup-2026?&io=transform:fill,aspectratio:1x1,width:1536&quality=75",
+    titulo: "🇪🇦 España arranca la conquista de la segunda estrella ante Cabo Verde",
+    imagen: "https://digitalhub.fifa.com/transform/bd0a6adc-adc8-4421-8332-f578ecbcf26b/Mikel-Oyarzabal-celebrates-scoring-for-Spain-against-Peru-at-the-Estadio-Cuauhtemoc-in-Puebla-in-a-FIFA-World-Cup-2026-warm-up-friendly?focuspoint=0.51,0.23&io=transform:fill,aspectratio:4x3,width:120&quality=75",
     categoria: "BOTA DE ORO",
-    url: "#"
-  },
-  {
-    titulo: "🇲🇽 México ya tiene listo su espectáculo para inaugurar el Mundial 2026.",
-    imagen: "https://i.ibb.co/Z69TCzdP/Getty-Images-2268851671-ee7486.png",
-    categoria: "INAUGURACION",
-    url: "#"
-  },
-  {
-    titulo: "Los estadios de EE.UU., México y Canadá listos para el torneo",
-    imagen: "https://i.ibb.co/23b3Jygb/01kam8pyjefjks6p7qqd.png",
-    categoria: "Estadios",
-    url: "#"
-  },
-  {
-    titulo: "Ronwen Williams: Sudáfrica aportará energía, alegría y estilo",
-    imagen: "https://digitalhub.fifa.com/transform/4101f77f-7be7-47d1-8af5-321297c3a98b/Ronwen-Williams-applauds-South-Africa-fans",
-    categoria: "Fixture",
-    url: "#"
-  },
-  {
-    titulo: "500 millones de solicitudes durante última fase para la compra de boletos",
-    imagen: "https://digitalhub.fifa.com/transform/7e47ea3b-bf2d-42ed-9bc7-837c2e4f11b7/FIFA-com_FWC26_Ticketing-Thumbnail_3840x2160",
-    categoria: "Entradas",
     url: "#"
   }
 ];
