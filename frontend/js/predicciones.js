042ece6b903fcf398fedbf532ea7c051cbf802ee
Viktor <elfotito@gmail.com>
2026-06-27 18:07:06 -0400

    Reemontada Teutooona

--- frontend/js/predicciones.js
+++ frontend/js/predicciones.js
@@ -275,7 +275,7 @@ function renderizarPartidos() {
     let partidosFiltrados;
     if (filtroFase === 'all') {
         partidosFiltrados = partidosPendientes;
-    } else if (filtroFase === '16avos') {
+    } else if (filtroFase === '16vos') {
         partidosFiltrados = partidosPendientes.filter(p => p.fase.startsWith('Grupo '));
     } else {
         partidosFiltrados = partidosPendientes.filter(p => p.fase === filtroFase);
